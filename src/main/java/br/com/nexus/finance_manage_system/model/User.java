01d467b4147c8b5442f2060018cbd43536fa350e
carloscarvalho <misterchaty@outlook.com>
2026-06-30 17:55:44 -0300

    Insert spring security

--- src/main/java/br/com/nexus/finance_manage_system/model/User.java
+++ src/main/java/br/com/nexus/finance_manage_system/model/User.java
@@ -1,4 +1,69 @@
 package br.com.nexus.finance_manage_system.model;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "USERS")
+public class User implements UserDetails {
+    @Id
+    private UUID id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column(name = "passwordhash")
+    private String passwordHash;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
